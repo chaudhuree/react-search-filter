902535905789b24f943b3c951f30ecb783f5e959
chaudhuree <chaudhuree00@gmail.com>
2022-09-17 11:12:13 +0600

    docs: import user data and set query

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Users } from './users';
 
 function App() {
-
+  const [query,setQuery]=useState(null)
+  console.log(query);
   return (
-    <div>chaudhuree</div>
+    <div className="app">
+      <input type="text" className="search" placeholder="search.." onChange={e=>setQuery(e.target.value)} />
+      <ul className="list">
+        {Users.map(user => <li className="listItem" key={user.id}>{user.first_name}</li>
+        )}
+
+        <li className="listItem">chaudhuree</li>
+      </ul>
+    </div>
   )
 }
 
