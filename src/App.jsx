f51ad3df9d6093ce6cec3a39e663c8bbf33330d5
chaudhuree <chaudhuree00@gmail.com>
2022-09-20 01:24:41 +0600

    docs: search functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { useState } from 'react';
-import './App.css';
-import { Users } from './users';
+import React, { useState } from "react";
+import "./App.css";
+import { Users } from "./users";
 
 function App() {
-  const [query,setQuery]=useState(null)
-    // const data= Users.filter(user=>user.first_name.toLowerCase().match(query))
-    // const data= Users.filter(user=>user.first_name.toLowerCase().includes(query))
+  const [query, setQuery] = useState(null);
+  // const data= Users.filter(user=>user.first_name.toLowerCase().match(query))
+  // const data= Users.filter(user=>user.first_name.toLowerCase().includes(query))
   // console.log(data);
   return (
     <div className="app">
-      <input type="text" className="search" placeholder="search.." onChange={e=>setQuery(e.target.value)} />
+      <input
+        type="text"
+        className="search"
+        placeholder="search.."
+        onChange={(e) => setQuery(e.target.value)}
+      />
       <ul className="list">
-        {Users.filter(user=>user.first_name.toLowerCase().includes(query.toLowerCase())).map(user => <li className="listItem" key={user.id}>{user.first_name}</li>
-        )}
+        {query &&
+          Users.filter((user) =>
+            user.first_name.toLowerCase().includes(query.toLowerCase())
+          ).map((user) => (
+            <li className="listItem" key={user.id}>
+              {user.first_name}
+            </li>
+          ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
