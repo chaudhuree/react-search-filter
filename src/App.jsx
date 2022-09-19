cf3559793ccdfc6252d62665e77002ee6e464b74
chaudhuree <chaudhuree00@gmail.com>
2022-09-20 00:03:58 +0600

    check with query

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,14 @@ import { Users } from './users';
 
 function App() {
   const [query,setQuery]=useState(null)
-  console.log(query);
+    // const data= Users.filter(user=>user.first_name?.match(query))
+  // console.log(data);
   return (
     <div className="app">
       <input type="text" className="search" placeholder="search.." onChange={e=>setQuery(e.target.value)} />
       <ul className="list">
         {Users.map(user => <li className="listItem" key={user.id}>{user.first_name}</li>
         )}
-
-        <li className="listItem">chaudhuree</li>
       </ul>
     </div>
   )
