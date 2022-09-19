b0db0be704d84471b2567bbff34440af5ac977ab
chaudhuree <chaudhuree00@gmail.com>
2022-09-20 01:58:17 +0600

    search by firstName lastName and email

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,23 @@ import { Users } from "./users";
 import Table from "./table";
 
 function App() {
-  const [query, setQuery] = useState(null);
+  const [query, setQuery] = useState("");
   // const data= Users.filter(user=>user.first_name.toLowerCase().match(query))
   // const data= Users.filter(user=>user.first_name.toLowerCase().includes(query))
   // console.log(data);
+
+  //  for multi-search function
+
+  const search = (data) => {
+    return data.filter((user) => {
+      return (
+        user.first_name.toLowerCase().includes(query.toLowerCase()) ||
+        user.last_name.toLowerCase().includes(query.toLowerCase()) ||
+        user.email.toLowerCase().includes(query.toLowerCase())
+      );
+    });
+  };
+  // console.log(search(Users));
   return (
     <div className="app">
       <input
@@ -16,17 +29,17 @@ function App() {
         placeholder="search.."
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {query &&
-          Users.filter((user) =>
-            user.first_name.toLowerCase().includes(query.toLowerCase())
-          ).map((user) => (
-            <li className="listItem" key={user.id}>
-              {user.first_name}
-            </li>
-          ))}
-      </ul>
-        <Table data={Users}/>
+      {/*<ul className="list">*/}
+      {/*  {query &&*/}
+      {/*    Users.slice(0,20).filter((user) =>*/}
+      {/*      user.first_name.toLowerCase().includes(query.toLowerCase())*/}
+      {/*    ).map((user) => (*/}
+      {/*      <li className="listItem" key={user.id}>*/}
+      {/*        {user.first_name}*/}
+      {/*      </li>*/}
+      {/*    ))}*/}
+      {/*</ul>*/}
+      <Table data={search(Users)} />
     </div>
   );
 }
--- src/table.jsx
+++ src/table.jsx
@@ -8,12 +8,14 @@ const Table = ({ data }) => {
           <th>Name</th>
           <th>Surname</th>
           <th>Email</th>
+          <th>Gender</th>
         </tr>
-        {data.slice(0,20).map((item) => (
+        {data.slice(0, 20).map((item) => (
           <tr key={item.id}>
             <td>{item.first_name}</td>
             <td>{item.last_name}</td>
             <td>{item.email}</td>
+            <td>{item.gender}</td>
           </tr>
         ))}
       </tbody>
