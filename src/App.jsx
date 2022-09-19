146cc34aa28ee263979649f22452b1ad3e7e238e
chaudhuree <chaudhuree00@gmail.com>
2022-09-20 00:08:46 +0600

    filter apply in list

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import { Users } from './users';
 
 function App() {
   const [query,setQuery]=useState(null)
-    // const data= Users.filter(user=>user.first_name?.match(query))
+    // const data= Users.filter(user=>user.first_name.toLowerCase().match(query))
+    // const data= Users.filter(user=>user.first_name.toLowerCase().includes(query))
   // console.log(data);
   return (
     <div className="app">
       <input type="text" className="search" placeholder="search.." onChange={e=>setQuery(e.target.value)} />
       <ul className="list">
-        {Users.map(user => <li className="listItem" key={user.id}>{user.first_name}</li>
+        {Users.filter(user=>user.first_name.toLowerCase().includes(query.toLowerCase())).map(user => <li className="listItem" key={user.id}>{user.first_name}</li>
         )}
       </ul>
     </div>
